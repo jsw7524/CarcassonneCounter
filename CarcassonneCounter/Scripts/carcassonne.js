827daa33aec172ae4ec7f68618a0d8f56c0723fc
jsw7524 <jsw7524@gmail.com>
2021-07-27 19:25:54 +0800

    Undo Click

--- CarcassonneCounter/Scripts/carcassonne.js
+++ CarcassonneCounter/Scripts/carcassonne.js
@@ -1,36 +1,44 @@
-    function countTotal() {
-      var total = 0;
+var stack = [];
 
-      $('table td span').each(function(){
-        total += parseInt( $(this).html() );
-      });
-      $('#total').html(total);
-    }
+function countTotal() {
+    var total = 0;
+    $('table td').each(function () {
 
-    $(document).ready(function(){
-      countTotal();
+        var el = $(this).find('span');
+        var n = parseInt($(el).html());
+        if (n === 0) {
+            $(this).css('opacity', '0.25');
+        }
+        else {
+            $(this).css('opacity', '1');
+        }
+        total += n;
+    });
+    $('#total').html(total);
+}
 
-      $('td').on('click', function(e){
+$(document).ready(function () {
+    countTotal();
+    $('td').on('click', function (e) {
         var el = $(this).find('span');
         var old = el.html();
-
-        if (e.shiftKey) {
-          var newv = 0 + parseInt(old) + 1;
-          el.html(newv);
-
-          if (newv != 0) {
-            $(this).css('opacity', '1');
-          }
-	} else  {
-          var newv = 0 + old - 1;
-          if (newv > -1) {
+        var newv = old - 1;
+        if (newv > -1) {
             el.html(newv);
-          }
-
-          if (newv == 0) {
-            $(this).css('opacity', '0.25');
-          }
+            stack.push(this)
         }
         countTotal();
-      });
     });
+
+    $('#button_undo').on('click', function (e) {
+        if (stack.length === 0) {
+            return;
+        }
+        var elmtd = stack.pop();
+        var el = $(elmtd).find('span');
+        var old = el.html();
+        var newv = parseInt(old) + 1;
+        el.html(newv);
+        countTotal();
+    });
+});
